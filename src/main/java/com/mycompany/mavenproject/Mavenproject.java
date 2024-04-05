4b0199e37948f2975be8a646b6b33dea16ff8cb5
ksusha-skvortsova <164548128+ksusha-skvortsova@users.noreply.github.com>
2024-04-06 02:11:16 +0300

    Mavenproject.java

--- src/main/java/com/mycompany/mavenproject/Mavenproject.java
+++ src/main/java/com/mycompany/mavenproject/Mavenproject.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Mavenproject {
 
     public static void main(String[] args) {
-        System.out.println("Practical task 1.12, Student Budrik Sophia, RIBO-04-22, Variant 4");
+        System.out.println("Practical task 1.12, Skvortsova Kseniya, RIBO-04-22, Variant 4");
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Please, enter name of plant:");
+        System.out.println("name of plant:");
         String value = scanner.nextLine();
 
-        System.out.println("Please, enter the plant family:");
+        System.out.println("plant family:");
         String value1 = scanner.nextLine();
         
-        System.out.println("Please, enter the color of the plant:");
+        System.out.println("color of the plant:");
         String value2 = scanner.nextLine();
 
-        System.out.println("Please, enter the age of the plant:");
+        System.out.println("age of the plant:");
         int value3 = scanner.nextInt();
 
-        System.out.println("Please, enter the height of the plant:");
+        System.out.println("height of the plant:");
         float value4 = scanner.nextFloat();
         
         Plant p = new Plant(value, value1, value2, value3, value4);
